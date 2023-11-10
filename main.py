94139f2c4ed08ab11b9f3a2421afa2aa62bf2391
Matthias Lapointe <matthias.lapointe@cpe.fr>
2023-11-10 11:14:49 +0100

    oui

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from player import player
 
+
 win = Tk()
 win.title('Space Invaders')
 win.attributes('-fullscreen', True)
 win['bg'] = 'black'
-
+player = player(win)
+player.can_player.place(relx=0.5, rely=0.9)
 win.mainloop()
--- player.py
+++ player.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from PIL import ImageTk
 
 class player:
-    def __init__(self):
-        self.health = 100
-        self.pos = (x,y)
-        self.damage = 5
-        self.can_player = Canvas(win , width = 300, height=300)
-        self.image_png = PhotoImage(file='image/player.gif')
-        self.player_item = self.can_player.create_image(300,300, image=self.image_png)
+    def __init__(self, win):
+        self.health = 100   
+        self.image = ImageTk.PhotoImage(file='image/player.png')  
+        self.can_player = Canvas(win , width=self.image.width(), height=self.image.height())
+        self.player_item = self.can_player.create_image(0,0,image=self.image)
+
+
+    def move(self):
+        pass
