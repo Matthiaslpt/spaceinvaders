37f17746fe5c14dd91971515a9d31b471ef05d95
Matthias Lapointe <matthias.lapointe@cpe.fr>
2023-11-10 09:25:41 +0100

    hop

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from window import window
+from tkinter import *
 from player import player
+
+win = Tk()
+win.title('Space Invaders')
+win.attributes('-fullscreen', True)
+win['bg'] = 'black'
+
+win.mainloop()
