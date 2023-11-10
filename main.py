c0c39e70e8db3cb290c3989dbef269eee227f8a6
Matthias Lapointe <matthias.lapointe@cpe.fr>
2023-11-10 11:56:23 +0100

    hop2

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from player import player
 
 
 win = Tk()
+w, h = win.winfo_screenwidth(), win.winfo_screenheight()
+win.geometry("%dx%d" % (w,h))
 win.title('Space Invaders')
 win.attributes('-fullscreen', True)
 win['bg'] = 'black'
 player = player(win)
-player.can_player.place(relx=0.5, rely=0.9)
+player.can_player.pack()
+
 win.mainloop()
--- player.py
+++ player.py
@@ -3,11 +3,16 @@ from PIL import ImageTk
 
 class player:
     def __init__(self, win):
-        self.health = 100   
+        self.health = 100 
+        self.pos = (0,0)
         self.image = ImageTk.PhotoImage(file='image/player.png')  
-        self.can_player = Canvas(win , width=self.image.width(), height=self.image.height())
+        self.can_player = Canvas(win , width=win.winfo_screenmmwidth(), height=win.winfo_screenmmwidth(), background='black')
         self.player_item = self.can_player.create_image(0,0,image=self.image)
-
-
-    def move(self):
-        pass
+        self.can_player.bind('<Key>', move)
+    def move(self,event):
+        touche = event.keysym
+        if touche == 'q':
+            self.pos[0] -= 15
+        elif touche == 'd':
+            self.pos[0] += 15
+        self.can_player.coords(self.player_item, self.pos[0],self.pos[1] )
