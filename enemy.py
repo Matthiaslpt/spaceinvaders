a121dd7f29c60e6533ec94c27755630d933a53e2
tajonathanh <tajonathanh@gmail.com>
2023-11-17 09:15:42 +0100

    enemy

--- enemy.py
+++ enemy.py
@@ -4,12 +4,14 @@ import random as r
 
 class enemy :
     def __init__(self,window) :
-        self.p=[r.randint(window.winfo_screenwidth()),200]
+        self.p=[r.randint(window.winfo_screenwidth(),window.winfo_screenwidth()),200]
         self.direction=1
         self.speed=2
-        self.can_enemy=Canvas(window, width=300, height=300)
+        larg = window.winfo_screenmmwidth()
+        haut=window.winfo_screenmmwidth()
+        self.can_enemy=Canvas(window , width=larg, height=haut, background='black')
         self.image_png = PhotoImage(file='image/alien.png')
-        self.enemy_item = self.can_enemy.create_image(0,0,image=self.image_png)
+        self.enemy_item = self.can_enemy.create_image(larg/2,haut/2,image=self.image_png)
 
     def move(self):
         if self.pos[0]<15 or self.pos[0]>1900:
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from player import player
+from enemy import enemy
 
 
 win = Tk()
@@ -10,5 +11,9 @@ win.attributes('-fullscreen', True)
 win['bg'] = 'black'
 player = player(win)
 player.can_player.pack()
+enemy = enemy(win)
+enemy.can_enemy.pack()
+quit=Button(win,text='Tchao', command=win.destroy)
+quit.pack(anchor='ne',side='top')
 
 win.mainloop()
